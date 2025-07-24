b5d977e3d6e7bbeed9f02b596111537bc8b25e91
Tanvir Hossain <tanvirsimonofficial@gmail.com>
2025-07-25 01:37:43 +0800

    Add files via upload

--- C-programming-basics/09. Pointer/Pass_by_reference.c
+++ C-programming-basics/09. Pointer/Pass_by_reference.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-void func(int *x) {
-    printf("x = %d\n", *x); // Before
-    printf("Address of x points to: %p\n", (void*)x);
-    printf("Address of x pointer variable: %p\n", (void*)&x); 
-    *x = 20;
-    printf("x = %d\n", *x); // After
-    printf("Address of x points to: %p\n", (void*)x);
-    printf("Address of x pointer variable: %p\n", (void*)&x); 
+void func(int *p) {
+    *p = 20;
+    
 }
 
 int main() {
-    int a = 10;
-    func(&a); // Pass by reference
-    printf("a = %d\n", a);
-    printf("Address of a: %p\n", &a);
+    int x = 20;
+    func(&x); // Pass by reference
+    printf("%d\n", x);
     return 0;
 }
--- C-programming-basics/09. Pointer/Pass_by_value.c
+++ C-programming-basics/09. Pointer/Pass_by_value.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 void func(int x) {
-    printf("x = %d\n", x); // Before
-    printf("Address of func function: %p\n", &x);
     x = 20;
-    printf("x = %d\n", x); // After
     printf("Address of func function: %p\n", &x);
 }
 
 int main() {
-    int a = 10;
-    func(a); // Pass by value
-    printf("a = %d\n", a);
-    printf("Address of main function: %p\n", &a);
+    int x = 10;
+    func(x); // Pass by value
+    printf("%d\n", x);
+    printf("Address of main function: %p\n", &x);
     return 0;
 }
